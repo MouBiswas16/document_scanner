60a30b8b2af060642262e88f51d937460fd6ce08
Mou Biswas <qtmeaowmou16@gmail.com>
2023-02-18 01:06:05 +0600

    comment deletion
    delete all the unnecessary parts of home_page.dart

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -31,26 +31,11 @@ class _HomePageState extends State<HomePage> {
             children: [
               SizedBox(height: 38),
               ElevatedButton(
-                // onPressed: () async {
-                //   await getImage(
-                //     image != null
-                //         ? Image.file(
-                //             File(image!.path),
-                //             // fit: BoxFit.fitWidth,
-                //           )
-                //         : Container(),
-                //   );
-                // },
                 onPressed: getImage,
                 child: Text("Get Image"),
               ),
               SizedBox(height: 30),
               ElevatedButton(
-                // onPressed: () async {
-                //   // scanText(image!.path as XFile);
-                //   //scanText(XFile(image!.path));
-                //   await scanText(image!);
-                // },
                 onPressed: scanText,
                 child: Text("Scan Object"),
               ),
@@ -83,17 +68,7 @@ class _HomePageState extends State<HomePage> {
 
     InputImage inputImage = InputImage.fromFilePath(image!.path);
     final textRecognizer = TextRecognizer();
-    final RecognizedText recognizedText =
-        await textRecognizer.processImage(inputImage);
-
-    // final inputImage = InputImage.fromFilePath(image.path);
-    // final textRecognizer = TextRecognizer();
-    // final recognizedText = await textRecognizer.processImage(inputImage);
-
-    // final textDetector = GoogleMlKit.vision.textDetector();
-    // RecognizedText recognizedText = await textDetector.processImage(inputImage);
-    // await textDetector.close();
-    // return recognizedText.text;
+    final recognizedText = await textRecognizer.processImage(inputImage);
 
     for (TextBlock block in recognizedText.blocks) {
       for (TextLine line in block.lines) {
@@ -104,27 +79,9 @@ class _HomePageState extends State<HomePage> {
     log("$scannedText");
     Navigator.of(context).pop();
     setState(() {});
-
-    // for (TextBlock block in recognizedText.blocks) {
-    //   setState(() {
-    //     scannedText += block.text + "\n";
-    //   });
-    // }
-
-    // Navigator.of(context).pop();
-    // Navigator.of(context).push(
-    //   MaterialPageRoute(
-    //     builder: (context) => ScannedText(
-    //       text: "",
-    //     ),
-    //   ),
-    // );
   }
 
   Future getImage() async {
-    // final pickedFile = await ImagePicker().pickImage(
-    //     source: image != null ? ImageSource.gallery : ImageSource.camera);
-
     final pickedFile =
         await ImagePicker().pickImage(source: ImageSource.gallery);
 
@@ -136,67 +93,4 @@ class _HomePageState extends State<HomePage> {
       }
     });
   }
-
-  // Future getImage() async {
-  //   var pickedImage;
-  //   showBottomSheet(
-  //     context: context,
-  //     builder: (context) {
-  //       return Column(
-  //         mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //         children: [
-  //           ListTile(
-  //             leading: Icon(
-  //               Icons.camera_alt_outlined,
-  //             ),
-  //             title: Text(
-  //               "Camera",
-  //             ),
-  //             onTap: () {
-  //               setState(() {
-  //                 pickedImage =
-  //                     ImagePicker().pickImage(source: ImageSource.camera);
-  //                 if (pickedImage != null) {
-  //                   image = pickedImage;
-  //                   scanText(image!);
-  //                 } else {
-  //                   final snackBar = SnackBar(
-  //                     content: Text(
-  //                       "No image selected",
-  //                     ),
-  //                   );
-  //                   // print("No image selected");
-  //                 }
-  //               });
-  //             },
-  //           ),
-  //           ListTile(
-  //             leading: Icon(
-  //               Icons.photo_library_rounded,
-  //             ),
-  //             title: Text(
-  //               "Photo Library",
-  //             ),
-  //             onTap: () {
-  //               setState(() {
-  //                 pickedImage =
-  //                     ImagePicker().pickImage(source: ImageSource.gallery);
-  //                 if (pickedImage != null) {
-  //                   scanText(image!);
-  //                 } else {
-  //                   final snackBar = SnackBar(
-  //                     content: Text(
-  //                       "No image selected",
-  //                     ),
-  //                   );
-  //                   // print("No image selected");
-  //                 }
-  //               });
-  //             },
-  //           ),
-  //         ],
-  //       );
-  //     },
-  //   );
-  // }
 }
